6d7ea3838163618ff6dffa04d7112616a41fead4
Samuel Sainz <samuel.sainz@mercadolibre.com>
2020-09-05 11:59:41 -0300

    RedditMate project created

---
+++ RedditMate/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditMate
+//
+//  Created by Samuel Sainz on 9/5/20.
+//  Copyright © 2020 Samuel Sainz. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
